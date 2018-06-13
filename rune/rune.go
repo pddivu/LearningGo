2a64a72b0559a2b2349beca4a1072243fac1605e
Divya <pddivu@gmail.com>
2018-06-13 08:27:27 -0700

    Rune concept

---
+++ rune/rune.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//UTF 8 int 4
+func main() {
+	for i := 100; i <= 140; i++ {
+		fmt.Println(i, "_", string(i), "_", []byte(string(i)))
+	}
+	// single qoutes 'a' indicates its a rune not a string
+	foo := 'a'
+	fmt.Println(foo)
+	fmt.Printf("%T\n", foo)
+	fmt.Printf("%T\n", rune(foo))
+}
